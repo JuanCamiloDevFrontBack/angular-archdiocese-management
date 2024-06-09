3e0a330b6c490d9f7b1b492d2c2163b75fc6aa35
JuanCamilo9915 <jcamiloduque0215@gmail.com>
2024-06-08 23:44:40 -0500

    Se realizan cambios en el proyecto referentes a configuraiones y organización de arquitectura del mismo.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,28 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styles: [`
+  .footer {
+    padding: 20px 0;
+    text-align: center;
+  }
+  
+  .footer a {
+    color: #333;
+    text-decoration: none;
+  }
+  `],
 })
 export class AppComponent {
   title = 'app-archdiocese-management';
+
+  /*constructor() {
+    this.translate.addLangs(['es', 'en']);
+    const lang = this.translate.getBrowserLang();
+    if (lang !== 'en' && lang !== 'es') {
+      this.translate.setDefaultLang('es');
+    } else {
+      this.translate.use(lang);
+    }
+  }*/
 }
