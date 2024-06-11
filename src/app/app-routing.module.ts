ef28291cb392a21a333dfed67bd5c93135c0c70a
JuanCamilo9915 <jcamiloduque0215@gmail.com>
2024-06-10 21:40:26 -0500

    Se restable la carga perezosa para el compoennete parish.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { existsParishGuard } from './core/guards/exists-parish.guard';
-import { AdminParishedComponent } from './features/admin-parished/admin-parished.component';
 
 const routes: Routes = [
-  { path: 'parished', component: AdminParishedComponent },
-  // {
-  //   path: 'parished',
-  //   loadComponent: () => import('./features/admin-parished/admin-parished.component')
-  //     .then(mod => mod.AdminParishedComponent)
-  // },
+  {
+    path: 'parished',
+    loadComponent: () => import('./features/admin-parished/admin-parished.component')
+      .then(mod => mod.AdminParishedComponent)
+  },
   {
     path: 'priests',
     loadComponent: () => import('./features/admin-priests/admin-priests.component')
